c70bace8e9d765e0ddb1ef19909a234ad543973a
Michael Tarleton <michael.tarleton@me.com>
2019-02-20 16:44:34 -0600

    master:  Added getData to user service

--- src/app/core/services/user-service.service.ts
+++ src/app/core/services/user-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as wjcCore from 'wijmo/wijmo';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,22 @@ import { Injectable } from '@angular/core';
 export class UserServiceService {
 
   constructor() { }
+
+  getData(count: number): wjcCore.ObservableArray {
+    const countries = 'US,Germany,UK,Japan,Italy,Greece'.split(',')
+    const data = new wjcCore.ObservableArray()
+
+    for (let i = 0; i < count; i++) {
+      data.push({
+        id: i,
+        country: countries[i % countries.length],
+        date: new Date(2014, i % 12, i % 28),
+        amount: Math.random() * 10000,
+        active: i % 4 === 0,
+        percent: Math.random() / 100
+      });
+    }
+
+    return data;
+  }
 }
